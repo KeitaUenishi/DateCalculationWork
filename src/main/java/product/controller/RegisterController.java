bd9f2de23e065269337d65b05c202a36bad46e1f
github KeitaUenishi <keita.uenishi@gmail.com>
2020-10-28 15:52:21 +0900

    新規登録処理実装まで

--- src/main/java/product/controller/RegisterController.java
+++ src/main/java/product/controller/RegisterController.java
@@ -1,11 +1,17 @@
 package product.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import product.domain.DateFormula;
+import product.service.CalculationServise;
 
 /*
  * 日付計算式を新規登録する画面
@@ -15,9 +21,34 @@ import product.domain.DateFormula;
 @RequestMapping("/register")
 public class RegisterController {
 
+	/** CalculationServiceに依存 */
+	@Autowired
+	private CalculationServise servise;
+
 	@GetMapping
 	public String index(@ModelAttribute DateFormula form) {
 		return "register";
+	}
+
+	/**
+	 * 日付計算式を新規登録する
+	 * 登録した後は一覧画面に戻る
+	 *
+	 * @param form 画面フォーム
+	 * @param model モデル
+	 * @return 表示するテンプレート
+	 */
+	@PostMapping
+	public String register(@ModelAttribute @Validated DateFormula form, BindingResult bindingResult, Model model) {
+
+		if (bindingResult.hasErrors()) {
+			return "register";
+		}
+
+		servise.register(form);
+		// model.addAttribute("simulationForm", new SimulationForm());
+		return "simulation";
+
 	}
 
 }
--- src/main/java/product/controller/SimulationController.java
+++ src/main/java/product/controller/SimulationController.java
@@ -1,11 +1,23 @@
 package product.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import product.service.CalculationServise;
 
 @Controller
 public class SimulationController {
 
+	/** CalculationServiseに依存 */
+	@Autowired
+	private CalculationServise servise;
+
+	/**
+	 * 初期表示処理
+	 * @param form 画面フォーム
+	 * @return 表示するテンプレート
+	 */
 	@GetMapping("/")
 	public String index() {
 		return "simulation";
