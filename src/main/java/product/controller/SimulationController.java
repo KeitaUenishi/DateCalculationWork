d1a4bc8d137e5bb7f70016876e0d4bb968dc82f2
github KeitaUenishi <keita.uenishi@gmail.com>
2020-11-03 17:14:35 +0900

    ログ出力の追加

--- src/main/java/product/controller/SimulationController.java
+++ src/main/java/product/controller/SimulationController.java
@@ -1,13 +1,27 @@
 package product.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import product.domain.Result;
+import product.domain.SimulationForm;
 import product.service.CalculationServise;
 
 @Controller
 public class SimulationController {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	/** CalculationServiseに依存 */
 	@Autowired
@@ -19,7 +33,59 @@ public class SimulationController {
 	 * @return 表示するテンプレート
 	 */
 	@GetMapping("/")
-	public String index() {
+	public String index(@ModelAttribute SimulationForm form) {
+		return "simulation";
+	}
+
+	/**
+	 * 日付計算式をもとにシミュレーション結果を一覧表示する
+	 *
+	 * @param form 画面フォーム
+	 * @param bindingResult 入力値検証の結果
+	 * @param model モデル
+	 * @return 表示するテンプレート
+	 */
+	@PostMapping
+	public String simulation(@ModelAttribute @Validated SimulationForm form,
+			BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "simulation";
+		}
+
+		/**
+		 * SimulationForm型のインスタンスを作成
+		 * SimlationFormの引数にgetBaseDate「計算基準日」、servise.serch「日付計算式の一覧を全件検索して取得する」
+		 */
+		SimulationForm resultForm = new SimulationForm(form.getBaseDate(), servise.search());
+		List<Result> results = resultForm.getResults();
+
+		//ラムダ式
+		results.stream().forEach(e -> e.setCalculated(servise.calculate(form.getBaseDate(), e.getFormula())));
+
+		/**
+		 * model.addAttributeとは
+		 * ・コントローラーからビュー側に値を渡す処理を行う場合に使う
+		 * 第一引数にテンプレートから参照する変数名、第二引数にオブジェクト名として格納
+		 */
+		model.addAttribute("results", results);
+
+		// dateformulaテーブルのデータをログに出力
+		logger.info("[dateformulaテーブルの中身]" + model.getAttribute("dateformula").toString());
+
+		return "simulation";
+	}
+
+	/**
+	 * 任意の日付計算式を削除する
+	 *
+	 * @param form 画面フォーム
+	 * @param model モデル
+	 * @return 表示するテンプレート
+	 */
+	@PostMapping(value = "/{dateId}")
+	public String delete(@PathVariable String dateId, Model model) {
+		servise.delete(dateId);
+		model.addAttribute("simulation", new SimulationForm());
 		return "simulation";
 	}
 
